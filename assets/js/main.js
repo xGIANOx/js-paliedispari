975a5575ec841379658c4c3a914a1dc0e1dde8ca
xGIANOx <studioenricogian@gmail.com>
2023-03-16 16:31:55 +0100

    [fix] code puntuaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,23 +40,35 @@ palindrome_finder(user_word, reversed_word);
 const user_team = prompt('pari o dispari?');
 console.log(user_team);
 
-const user_number = Number(prompt('Scegli un numero da 1 a 5!'))
+const user_number = Number(prompt('Scegli un numero da 1 a 5!'));
 console.log(user_number);
 
 function computer_number () {
     let pc_number = Math.floor(Math.random() * 5) + 1;
     console.log(pc_number);
-    return pc_number
+    return pc_number;
 }
 
 
 const sum = user_number + computer_number();
 console.log(sum);
 
-if (user_team == 'pari' && sum % 2 == 0){
-    console.log('Pari. Hai vinto!');
-} else if (user_team == 'dispari' && sum % 2 !== 0){
-    console.log('Dispari. Hai vinto!');
-} else {
-    console.log('Hai perso');
+// if (user_team == 'pari' && sum % 2 == 0){
+//     console.log('Pari. Hai vinto!');
+// } else if (user_team == 'dispari' && sum % 2 !== 0){
+//     console.log('Dispari. Hai vinto!');
+// } else {
+//     console.log('Hai perso');
+// }
+
+function even_odd_game () {
+    if (user_team == 'pari' && sum % 2 == 0){
+        console.log('Pari. Hai vinto!');
+    } else if (user_team == 'dispari' && sum % 2 !== 0){
+        console.log('Dispari. Hai vinto!');
+    } else {
+        console.log('Hai perso');
+    }
 }
+
+even_odd_game ();
